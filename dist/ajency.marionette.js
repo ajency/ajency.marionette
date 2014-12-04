9571bf5e0be9bddfacf83ba562dfcb415725d365
Suraj Air <suraj@ajency.in>
2014-12-04 18:57:24 +0530

    v0.1.2 added

--- dist/ajency.marionette.js
+++ dist/ajency.marionette.js
@@ -4,7 +4,7 @@
  * Ajency.Marionette
  * https://github.com/ajency/ajency.marionette/wiki
  * --------------------------------------------------
- * Version: v0.1.1
+ * Version: v0.1.2
  *
  * Copyright(c) 2014 Team Ajency, Ajency.in
  * Distributed under MIT license
